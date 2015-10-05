9d9138e75961c523abffabd3dfcc24232d17599a
avengineer <avengineer@lf.lc>
2015-10-05 12:28:55 -0500

    Sample code for robot movement within server, needs testing

--- ros_alfred/src/alfred_server/src/server.py
+++ ros_alfred/src/alfred_server/src/server.py
@@ -9,13 +9,16 @@ from StringIO import StringIO
 import rospy
 from std_msgs.msg import String
 
+from actionlib.action_client import *
+from move_base_msgs.msg import *
+ 
 app = Flask(__name__)
 pub = rospy.Publisher('chatter', String, queue_size=10)
 rospy.init_node('talker', anonymous=True)
 rospy.loginfo("ros node initialized")
 
 MAP_FILE = "/home/avengineer//alfred_ws/ros_alfred/src/alfred_maps/maps/uta_basement.pgm"
-inventory = {"Water":10, "Granola":5}
+inventory = {"water":10, "granola":5}
 rooms = ["living room", "kitchen", "dining room"]
 
 def serve_pil_image(pil_img):
@@ -55,6 +58,34 @@ def items():
 
     inventory[item] -= 1
     return jsonify({"name":item, "quantity":inventory[item]})
+
+# TODO: move to state machine
+def move(x, y):
+    width = 1984
+    height = 1984
+    resolution = 0.05
+    origin = [-50, -50]
+
+    x -= width / 2
+    y -= height / 2
+
+    x -= origin[0] * resolution
+    y -= origin[1] * resolution
+
+    ac = ActionClient('move_base', MoveBaseAction)
+    ac.wait_for_server()
+    rospy.loginfo("Got move_base client")
+
+    goal = MoveBaseGoal()
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.orientation.w = 1.0
+    goal.target_pose.header.frame_id = 'map'
+
+    rospy.loginfo("Sending goal")
+    ac.send_goal(goal)
+    ac.wait_for_server()
+    rospy.loginfo("Goal reached")
 
 def signal_handler(signal, frame):
     sys.exit(0)
