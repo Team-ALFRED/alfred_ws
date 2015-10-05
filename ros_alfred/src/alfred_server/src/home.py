7693bd7ee7415a16b3c2753ac6e07b62738957f5
avengineer <avengineer@lf.lc>
2015-10-05 12:31:38 -0500

    Home script takes coords from command line

--- ros_alfred/src/alfred_server/src/home.py
+++ ros_alfred/src/alfred_server/src/home.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 from actionlib.action_client import *
 from move_base_msgs.msg import *
+import sys
+
+coords = [0.0, 0.0]
+if len(sys.argv) == 3:
+  coords[0] = float(sys.argv[1])
+  coords[1] = float(sys.argv[2])
 
 rospy.init_node('move_base_client')
 ac = ActionClient('move_base', MoveBaseAction)
@@ -8,8 +14,8 @@ ac.wait_for_server()
 rospy.loginfo("Got move_base client")
 
 goal = MoveBaseGoal()
-goal.target_pose.pose.position.x = -4.0
-goal.target_pose.pose.position.y = 0.0
+goal.target_pose.pose.position.x = coords[0]
+goal.target_pose.pose.position.y = coords[1]
 goal.target_pose.pose.orientation.w = 1.0
 goal.target_pose.header.frame_id = 'map'
 
