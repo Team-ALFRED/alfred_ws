0295246fdd20551884ade54bd50a5a55b24d7e8c
avengineer <avengineer@lf.lc>
2015-10-28 14:44:01 -0500

    Dispenser and config changes

---
+++ ros_alfred/src/alfred_server/src/alfred_config.py
@@ -0,0 +1,7 @@
+
+# Dispenser inventory, edit to reflect initial state
+DEFAULT_INV = {"water":4, "granola":4}
+
+# Authentication token - modify for personal use
+SECRET_TOKEN="JWe2fxjbR"
+
--- ros_alfred/src/alfred_server/src/dispenser.py
+++ ros_alfred/src/alfred_server/src/dispenser.py
@@ -2,7 +2,7 @@ import socket
 import time
 import sys
 
-DISPENSER, PORT = "192.168.0.2", 5001
+DISPENSER, PORT = "192.168.0.3", 5001
 TIMEOUT = 8.0
 
 def req(item):
@@ -11,7 +11,6 @@ def req(item):
   sock.close()
 
 def ack():
-  return True
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   sock.settimeout(TIMEOUT)
   sock.bind(("0.0.0.0", PORT))
