0d50165c67021d420ed5dd380231598725d02e6c
Anthony Weems <amlweems@gmail.com>
2015-11-18 12:19:57 -0600

    Update default inventory

--- ros_alfred/src/alfred_server/src/alfred_config.py
+++ ros_alfred/src/alfred_server/src/alfred_config.py
@@ -1,6 +1,6 @@
 
 # Dispenser inventory, edit to reflect initial state
-DEFAULT_INV = {"water":4, "granola":4}
+DEFAULT_INV = {"water":4, "reeses":4}
 
 # Authentication token - modify for personal use
 SECRET_TOKEN="JWe2fxjbR"
